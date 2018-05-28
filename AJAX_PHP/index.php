d3a2ebfffb28fa9ff1241adac9dcbed11ccd7156
logankratos61 <32728067+logankratos61@users.noreply.github.com>
2018-05-29 02:05:06 +0530

    Update index.php

--- AJAX_PHP/index.php
+++ AJAX_PHP/index.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title> Search Box</title>
-     <link  rel="stylesheet" href="https://bootswatch.com/4/cerulean/bootstrap.min.css">
+     <link  rel="stylesheet" href="https://bootswatch.com/4/cerulean/bootstrap.min.css"> //Bootstarp theme taken form bootswatch.com.
      <script>
         function showSuggestion(str){
                
