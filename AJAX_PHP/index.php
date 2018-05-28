d53dc28dca522bcf7f5c2767628331dec1ed8b01
logankratos61 <32728067+logankratos61@users.noreply.github.com>
2018-05-29 01:55:45 +0530

    Create index.php

---
+++ AJAX_PHP/index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title> Search Box</title>
+     <link  rel="stylesheet" href="https://bootswatch.com/4/cerulean/bootstrap.min.css">
+     <script>
+        function showSuggetion(str){
+               
+            if(str.length == 0){
+                document.getElementById('output').innerHTML = '';
+            }else{
+                //AJAX REQUEST
+                var xhr = new XMLHttpRequest();
+                xhr.open("GET", "suggest.php?q="+str, true );
+                xhr.onload = function(){
+                    if(this.status==200){
+                        document.getElementById('output').innerHTML = this.responseText;
+                    }
+                }
+                xhr.send();
+            }   
+
+             }
+     </script>
+</head>
+<body>
+<div class="container">
+
+    <h1>Search User</h1> 
+    <br><br>
+    <form>
+    Search user <input type="text" class="form-control" onkeyup="showSuggetion(this.value)">
+    </form>
+    <br><br>
+    <p>Suggetions: <span id="output" style="font-weight:bold"></span></p>
+    </div>
+    
+</body>
+</html>
